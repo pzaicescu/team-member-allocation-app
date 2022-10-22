b6465283563059f8847a7490f7f945b7f0afda59
Zaicescu Paul <paul.zaicescu@tickbird.org>
2022-10-22 17:07:18 +0300

    adding React Router

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <header className="container">
+            <div className="row justify-content-center mt-3 mb-4">
+                <h1 className="text-danger">404 - Page not found</h1>
+            </div>
+        </header>
+    );
+};
+
+export default NotFound;
